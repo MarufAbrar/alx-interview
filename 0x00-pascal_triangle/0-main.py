baa4a290d26700c9579f85724a90fc8955e887d1
MarufAbrar <maracode1@gmail.com>
2023-01-25 22:44:48 +0300

    alx is fun

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -15,10 +15,3 @@ def print_triangle(triangle):
 if __name__ == "__main__":
     print_triangle(pascal_triangle(5))
 
-guillaume@ubuntu:~/0x00$ 
-guillaume@ubuntu:~/0x00$ ./0-main.py
-[1]
-[1,1]
-[1,2,1]
-[1,3,3,1]
-[1,4,6,4,1]
