c952a4e749f80617c1567afa5796398d451ddb28
MarufAbrar <maracode1@gmail.com>
2023-01-25 22:51:48 +0300

    alx is fun

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,26 +1,27 @@
-def pascal_triangle(n): 
-    result = [] 
+#!/usr/bin/python3
+"""Pascal Triangle Interview Challenge"""
 
-    # Edge case 
-    if n <= 0: 
-        return result 
 
-    # Generate an empty list of lists  
-    for rows in range(n): 
-        row_list = []  
-        for cols in range(rows + 1): 
-            row_list.append(0)  
-        result.append(row_list) 
+def pascal_triangle(n):
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
+    if n <= 0:
+        return []
 
-    # Update the first and last elements to 1 
-    result[0][0] = 1
-    for rows in range(1, n): 
-        result[rows][0] = 1
-        result[rows][rows] = 1
+    pascal_triangle = [0] * n
 
-    # Update the values inside the list of lists using the previous row 
-    for rows in range(1, n): 
-        for cols in range(1, rows): 
-            result[rows][cols] = result[rows-1][cols-1] + result[rows-1][cols] 
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
 
-    return result 
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
