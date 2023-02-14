e4bc25bb80be1e5cc309ffabc58a8ff6be738e27
MarufAbrar <maracode1@gmail.com>
2023-02-14 14:37:25 +0300

    fun interview

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+
+total_size = 0
+count_by_status = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+for line in sys.stdin:
+    # Parse the line
+    line_parts = line.split()
+    if len(line_parts) != 5:
+        continue
+    ip_address = line_parts[0]
+    date = line_parts[1]
+    status_code = int(line_parts[3])
+    file_size = int(line_parts[4])
+
+    # Update the total size
+    total_size += file_size
+
+    # Update the count by status
+    if status_code in count_by_status:
+        count_by_status[status_code] += 1
+
+    # Print the stats after every 10 lines
+    if line_count % 10 == 0:
+        print('Total file size:', total_size)
+        for status_code in sorted(count_by_status):
+            print('{}: {}'.format(status_code, count_by_status[status_code]))
